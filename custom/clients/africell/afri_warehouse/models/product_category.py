1be0cc2d06448078994bea30c962a8c2d20477b9
butagreeza <fbangura@idtlabs.xyz>
2018-10-30 20:54:22 +0000

    Added Spare parts

--- custom/clients/africell/afri_warehouse/models/product_category.py
+++ custom/clients/africell/afri_warehouse/models/product_category.py
@@ -1,7 +1,22 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
     _rec_name = 'name'
     is_category = fields.Boolean(string='Top Category', default=False)
+    is_sub_category = fields.Boolean(string='Sub Category', default=False)
+    category_code = fields.Char(string='Category Code')
+    sub_category_code = fields.Char(string='Sub Category Code')
+
+
+    @api.multi
+    def get_category(self):
+        self.ensure_one()
+        category_object = self.env['product.category'].search([('is_category', '=', True)])
+        sub_category_object = self.env['product.category'].search([('is_sub_category', '=', True)])
+        # Lets get all the categories that are of type category
+        for rec in sub_category_object:
+            category = category_object.search([('category_code', '=', rec.category_code)])
+            rec.parent_id = category and category.id
+
--- custom/clients/africell/afri_warehouse/models/product_product.py
+++ custom/clients/africell/afri_warehouse/models/product_product.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductProduct(models.Model):
@@ -6,3 +6,24 @@ class ProductProduct(models.Model):
     department_ids = fields.Many2many(comodel_name='warehouse.department',
                                       rrequired=True,
                                       string='Applicable Departments')
+    product_code = fields.Char(string='Spare Part Code', copy=False)
+    sub_category_code = fields.Char(string='Sub Category Code')
+
+
+    @api.multi
+    def get_parent(self):
+        category_object = self.env['product.category']
+        all_products = self.search([])
+            # Lets get all the categories that are of type category
+        for rec in all_products:
+            if rec.sub_category_code:
+                sub_category = category_object.search([('sub_category_code', '=', rec.sub_category_code)])
+                rec.categ_id = sub_category and sub_category.id
+
+        p_category_object = self.env['product.category'].search([('is_category', '=', True)])
+        # Lets get all the categories that are of type category
+        for rec in p_category_object:
+            category = category_object.search([('category_code', '=', rec.category_code)])
+            rec.parent_id = category and category.id
+
+
